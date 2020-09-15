d550ca09cf874e4b3106181e17c18d72c1c6b0f0
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 02:09:52 +0530

    Add files via upload

---
+++ app01/dev/dev_alb.tf
@@ -0,0 +1,10 @@
+module "alb" {
+  source = "./alb"
+  vpc_id = "${module.vpc.vpc_id}"
+
+    instance1_id = "${module.ec2.instance1_id}"
+      instance2_id = "${module.ec2.instance2_id}"
+  subnet1 = "${module.vpc.subnet1}"
+
+  subnet2 = "${module.vpc.subnet2}"
+}
