25a0bdf9a2e46b49c6d63c6114e885af20b5bccc
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 06:10:48 +0530

    Update alb_listner.tf

--- modules/alb/alb_listner.tf
+++ modules/alb/alb_listner.tf
@@ -1,10 +1,10 @@
 resource "aws_lb_listener" "my-test-alb-listner" {
-  load_balancer_arn = "${aws_lb.my-aws-alb.arn}"
+  load_balancer_arn = "aws_lb.my-aws-alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.my-target-group.arn}"
+    target_group_arn = aws_lb_target_group.my-target-group.arn
   }
 }
