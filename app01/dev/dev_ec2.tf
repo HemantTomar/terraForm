8c0ebbfa2d8b241e4f3aa525d698163c099d6232
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 06:01:44 +0530

    Update dev_ec2.tf
    committed

--- app01/dev/dev_ec2.tf
+++ app01/dev/dev_ec2.tf
@@ -3,6 +3,6 @@ module "ec2" {
   source         = "./ec2"
   my_public_key  = "/home/ec2-user/.ssh/id_rsa.pub"
   instance_type  = "t2.micro"
-  security_group = "${module.vpc.security_group}"
-  subnets        = "${module.vpc.public_subnets}"
+  security_group = module.vpc.security_group
+  subnets        = module.vpc.public_subnets
 }
