5686efe7e573966266de1084ae3db618b3fb069e
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 06:11:58 +0530

    Update alb_target_group.tf

--- modules/alb/alb_target_group.tf
+++ modules/alb/alb_target_group.tf
@@ -12,5 +12,5 @@ resource "aws_lb_target_group" "my-target-group" {
   port        = 80
   protocol    = "HTTP"
   target_type = "instance"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 }
