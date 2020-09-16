625f4c5dfb9ae64eea38913ab01ecc0879cb5566
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 06:15:29 +0530

    Update outbound_rules.tf

--- modules/alb/outbound_rules.tf
+++ modules/alb/outbound_rules.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group_rule" "outbound_all" {
   from_port         = 0
   protocol          = "-1"
-  security_group_id = "${aws_security_group.my-alb-sg.id}"
+  security_group_id = aws_security_group.my-alb-sg.id
   to_port           = 0
   type              = "egress"
   cidr_blocks       = ["0.0.0.0/0"]
