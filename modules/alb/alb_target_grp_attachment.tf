e49721453b46e02525bb04ead956beef91290810
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 06:12:59 +0530

    Update alb_target_grp_attachment.tf

--- modules/alb/alb_target_grp_attachment.tf
+++ modules/alb/alb_target_grp_attachment.tf
@@ -1,10 +1,10 @@
 resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment1" {
-  target_group_arn = "${aws_lb_target_group.my-target-group.arn}"
-  target_id        = "${var.instance1_id}"
+  target_group_arn = aws_lb_target_group.my-target-group.arn
+  target_id        = var.instance1_id
   port             = 80
 }
 resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment2" {
-  target_group_arn = "${aws_lb_target_group.my-target-group.arn}"
-  target_id        = "${var.instance2_id}"
+  target_group_arn = aws_lb_target_group.my-target-group.arn
+  target_id        = var.instance2_id
   port             = 80
 }
