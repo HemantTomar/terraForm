cabb16d2afa2e3f4d20acabaad03808582655fe0
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 06:00:22 +0530

    Update dev_alb.tf
     commited

--- app01/dev/dev_alb.tf
+++ app01/dev/dev_alb.tf
@@ -1,10 +1,8 @@
 module "alb" {
   source = "./alb"
-  vpc_id = "${module.vpc.vpc_id}"
-
-    instance1_id = "${module.ec2.instance1_id}"
-      instance2_id = "${module.ec2.instance2_id}"
-  subnet1 = "${module.vpc.subnet1}"
-
-  subnet2 = "${module.vpc.subnet2}"
+  vpc_id = module.vpc.vpc_id
+  instance1_id = module.ec2.instance1_id
+  instance2_id = module.ec2.instance2_id
+  subnet1 = module.vpc.subnet1
+  subnet2 = module.vpc.subnet2
 }
