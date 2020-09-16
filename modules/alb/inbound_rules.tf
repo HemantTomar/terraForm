0d1b5ea0587b40233d69ab849fed91a83cf35d6b
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2020-09-16 06:14:02 +0530

    Update inbound_rules.tf

--- modules/alb/inbound_rules.tf
+++ modules/alb/inbound_rules.tf
@@ -2,7 +2,7 @@
 resource "aws_security_group_rule" "inbound_ssh" {
   from_port         = 22
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.my-alb-sg.id}"
+  security_group_id = aws_security_group.my-alb-sg.id
   to_port           = 22
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
@@ -11,7 +11,7 @@ resource "aws_security_group_rule" "inbound_ssh" {
 resource "aws_security_group_rule" "inbound_http" {
   from_port         = 80
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.my-alb-sg.id}"
+  security_group_id = aws_security_group.my-alb-sg.id
   to_port           = 80
   type              = "ingress"
   cidr_blocks       = ["0.0.0.0/0"]
